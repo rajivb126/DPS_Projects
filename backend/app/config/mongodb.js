dbbf43d591c63f28a7f4e44aae51129e2e5c520a
rajivb126 <134359267+rajivb126@users.noreply.github.com>
2024-12-12 17:44:13 +0530

    Update mongodb.js

--- backend/app/config/mongodb.js
+++ backend/app/config/mongodb.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/dpsjodhpurproject');
+mongoose.connect('mongodb+srv://rajiv:DPSj@9414247706#jodhpur@cluster0.e7fj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
