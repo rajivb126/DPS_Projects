8bb1071dea1baeae328e4f5b2994c0f734a360a0
Harshit <harshit.mathur.official@gmail.com>
2024-12-13 13:11:25 +0530

    reupdate login.jsx

--- frontend/src/admin/auth/Login.jsx
+++ frontend/src/admin/auth/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
         try {
             const response = await axios.post(`${API_BASE_URL}/login`, { username, password });
             if (response.data.success) {
-                localStorage.getItem('isAuthenticated', 'true');
+                localStorage.setItem('isAuthenticated', 'true');
                 setError('');
                 navigate('/dashboard');
             }
