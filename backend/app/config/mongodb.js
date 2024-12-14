84a9ba32944a5e49871feb2cd5396aeaeb2bf347
Harshit <harshit.mathur.official@gmail.com>
2024-12-14 13:46:19 +0530

    some update in mongodb.js

--- backend/app/config/mongodb.js
+++ backend/app/config/mongodb.js
@@ -1,5 +1,15 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0');
+const mongoURI ='mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0';
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    serverSelectionTimeoutMS: 30000, // Increase timeout
+})
+.then(() => console.log('MongoDB connected successfully!'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 // mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
