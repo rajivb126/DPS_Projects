e9971f83e004427b9a559b99b7385adf43534da9
Harshit <harshit.mathur.official@gmail.com>
2024-12-14 14:47:57 +0530

    update congif file in backend

--- backend/app/config/mongodb.js
+++ backend/app/config/mongodb.js
@@ -1,16 +1,3 @@
 const mongoose = require('mongoose')
 
-const mongoURI ='mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0';
-
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false,
-    serverSelectionTimeoutMS: 30000, // Increase timeout
-    bufferTimeoutMS: 30000, // Increase buffer timeout
-})
-.then(() => console.log('MongoDB connected successfully!'))
-.catch((err) => console.error('MongoDB connection error:', err));
-
-// mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+mongoose.connect('mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0');
