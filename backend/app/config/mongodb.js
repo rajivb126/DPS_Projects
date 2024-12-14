0aff909c64bd2a2d11dbfa979180223f97aaaf77
Harshit <harshit.mathur.official@gmail.com>
2024-12-14 12:29:22 +0530

    mongoose connection update

--- backend/app/config/mongodb.js
+++ backend/app/config/mongodb.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://rajiv:DPSj@9414247706#jodhpur@cluster0.e7fj7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');''
+mongoose.connect('mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/mernstack?retryWrites=true&w=majority&appName=Cluster0');
+
+// mongodb+srv://rajiv:TOS9f6DqcXCBjLsl@cluster0.zjjjg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
