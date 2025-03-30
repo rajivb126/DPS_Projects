35aa8d1d23847691dbd35d50665648a2d1d08979
Harshit <harshit.mathur.official@gmail.com>
2025-03-30 14:01:47 +0530

    upload function edit

--- backend/app/controllers/faculty.controller.js
+++ backend/app/controllers/faculty.controller.js
@@ -1,9 +1,10 @@
 const faculty = require('../models/faculty');
 const multer = require('multer');
+const path = require('path')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads')
+        cb(null, path.join(__dirname, 'uploads'));
     },
     filename: function (req, file, cb) {
         const uniqueFileName = (new Date().getTime())
