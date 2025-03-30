1660bcf68d1601eab9536a9737ac3c184170dacd
Harshit <harshit.mathur.official@gmail.com>
2025-03-30 14:21:03 +0530

    add path file include

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const imageUpload = require('./app/library/multer');
 var cors = require('cors')
 const app = express();
+const path = require('path');
 const mongoose = require('mongoose');
 
 // MongoDB connection string
